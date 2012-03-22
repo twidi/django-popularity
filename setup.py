a4ecaebd261482a326171dbc33e5133c4698c9b3
twidi <s.angel@twidi.com>
2012-03-22 19:13:13 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 # This file is part of django-popularity.
-# 
-# django-popularity: A generic view- and popularity tracking pluggable for Django. 
+#
+# django-popularity: A generic view- and popularity tracking pluggable for Django.
 # Copyright (C) 2008-2010 Mathijs de Bruin
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -25,7 +25,7 @@ except:
 
 setup(
     name = 'django-popularity',
-    version = "0.1.6",
+    version = "0.1.7",
     description = 'A generic view- and popularity tracking pluggable for Django.',
     long_description = README,
     author = 'Mathijs de Bruin',
